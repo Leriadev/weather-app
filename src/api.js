9b85af75231a4677254ba40af4015d91e07798d7
slurpgoodsoup <sloonaperchonka@gmail.com>
2022-08-27 14:52:40 +0300

    search component

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5fa8e1c81fmshf15c3855b3a0145p13fa3bjsnb2cd4750d578',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
